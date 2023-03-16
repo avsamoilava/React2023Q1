52993af5b0e2cb1fdf40d9eb17a72c773af9a5e6
Alexandra Samoilova <avsamoilava@gmail.com>
2023-03-16 21:42:24 +0300

    feat: add cards

--- react-components/src/pages/MainPage.tsx
+++ react-components/src/pages/MainPage.tsx
@@ -1,5 +1,6 @@
 import { Search } from '../components/UI/Search';
 import React from 'react';
+import { products, Product } from '../data/data';
 
 export class MainPage extends React.Component {
   render() {
@@ -7,6 +8,22 @@ export class MainPage extends React.Component {
       <div className="page main">
         <div className="main__search">
           <Search />
+        </div>
+        <div className="main__catalog">
+          <div className="catalog">
+            {products.map((prod: Product) => {
+              return (
+                <div className="catalog__card card" key={prod.id}>
+                  <div className="card__img">
+                    <img src={prod.image} alt={`${prod.title} photo`} />
+                  </div>
+                  <div className="card__title">{prod.title}</div>
+                  <div className="card__category">{prod.category}</div>
+                  <div className="card__price">{prod.price}€</div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     );
