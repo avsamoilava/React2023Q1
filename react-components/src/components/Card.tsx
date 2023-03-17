bdde0fe4e1764adf76d4a63ffd6871a4b22a6b3f
Alexandra Samoilova <avsamoilava@gmail.com>
2023-03-17 18:44:54 +0300

    refactor: add Card component

---
+++ react-components/src/components/Card.tsx
@@ -0,0 +1,27 @@
+import { Product } from 'data/data';
+import React from 'react';
+
+type CardProps = {
+  product: Product;
+};
+
+export class Card extends React.Component<CardProps> {
+  product: Product;
+
+  constructor(props: CardProps) {
+    super(props);
+    this.product = props.product;
+  }
+  render() {
+    return (
+      <div className="catalog__card card" key={this.product.id}>
+        <div className="card__img">
+          <img src={this.product.image} alt={`${this.product.title} photo`} />
+        </div>
+        <div className="card__title">{this.product.title}</div>
+        <div className="card__category">{this.product.category}</div>
+        <div className="card__price">{this.product.price}€</div>
+      </div>
+    );
+  }
+}
--- react-components/src/pages/MainPage.tsx
+++ react-components/src/pages/MainPage.tsx
@@ -1,6 +1,7 @@
 import { Search } from '../components/UI/Search';
 import React from 'react';
 import { products, Product } from '../data/data';
+import { Card } from '../components/Card';
 
 export class MainPage extends React.Component {
   render() {
@@ -11,18 +12,9 @@ export class MainPage extends React.Component {
         </div>
         <div className="main__catalog">
           <div className="catalog">
-            {products.map((prod: Product) => {
-              return (
-                <div className="catalog__card card" key={prod.id}>
-                  <div className="card__img">
-                    <img src={prod.image} alt={`${prod.title} photo`} />
-                  </div>
-                  <div className="card__title">{prod.title}</div>
-                  <div className="card__category">{prod.category}</div>
-                  <div className="card__price">{prod.price}€</div>
-                </div>
-              );
-            })}
+            {products.map((prod: Product) => (
+              <Card key={prod.id} product={prod} />
+            ))}
           </div>
         </div>
       </div>
