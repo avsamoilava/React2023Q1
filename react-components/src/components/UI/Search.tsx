e9b76f43f3b4859d8b51f31f2b99e3f92c521cea
Alexandra Samoilova <avsamoilava@gmail.com>
2023-03-29 22:21:28 +0300

    feat: change class on function in Search

--- react-components/src/components/UI/Search.tsx
+++ react-components/src/components/UI/Search.tsx
@@ -1,47 +1,30 @@
 import { SearchIcon } from '../icons/SearchIcon';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-type SearchState = {
-  value: string;
+export const Search = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    setSearchValue(localStorage.getItem('searchValue') || '');
+  }, [searchValue]);
+
+  const changeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    localStorage.setItem('searchValue', event.currentTarget.value);
+  };
+
+  return (
+    <form className="search">
+      <div className="search__icon">
+        <SearchIcon />
+      </div>
+      <input
+        className="search__input"
+        type="search"
+        defaultValue={searchValue}
+        placeholder="enter for search..."
+        onChange={changeValue}
+        role="search-input"
+      />
+    </form>
+  );
 };
-
-export class Search extends React.Component {
-  state: SearchState;
-
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      value: '',
-    };
-  }
-
-  componentDidMount(): void {
-    this.setState({ value: localStorage.getItem('searchValue') || '' });
-  }
-
-  componentWillUnmount(): void {
-    localStorage.setItem('searchValue', this.state.value);
-  }
-
-  changeValue(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ value: event.currentTarget.value });
-  }
-
-  render() {
-    return (
-      <form className="search">
-        <div className="search__icon">
-          <SearchIcon />
-        </div>
-        <input
-          className="search__input"
-          type="search"
-          defaultValue={this.state.value}
-          placeholder="enter for search..."
-          onChange={(e) => this.changeValue(e)}
-          role="search-input"
-        />
-      </form>
-    );
-  }
-}
