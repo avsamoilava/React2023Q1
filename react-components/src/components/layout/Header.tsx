b8762432c59846645c9affd60f510d59270151fe
Alexandra Samoilova <avsamoilava@gmail.com>
2023-03-29 20:39:42 +0300

    feat: change class on function in Header

--- react-components/src/components/layout/Header.tsx
+++ react-components/src/components/layout/Header.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export class Header extends React.Component {
-  render() {
-    return (
-      <header className="header">
-        <nav className="header__nav header-nav">
-          <ul className="header-nav__list">
-            <NavLink className="header-nav__link" to={'/'}>
-              Main
-            </NavLink>
-            <NavLink className="header-nav__link" to={'/about'}>
-              About us
-            </NavLink>
-            <NavLink className="header-nav__link" to={'/form'}>
-              Form
-            </NavLink>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-}
+export const Header = () => {
+  return (
+    <header className="header">
+      <nav className="header__nav header-nav">
+        <ul className="header-nav__list">
+          <NavLink className="header-nav__link" to={'/'}>
+            Main
+          </NavLink>
+          <NavLink className="header-nav__link" to={'/about'}>
+            About us
+          </NavLink>
+          <NavLink className="header-nav__link" to={'/form'}>
+            Form
+          </NavLink>
+        </ul>
+      </nav>
+    </header>
+  );
+};
