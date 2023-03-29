85be2c68353f40476d402e6e378ec1450b2e64d6
Alexandra Samoilova <avsamoilava@gmail.com>
2023-03-29 22:35:48 +0300

    feat: change class on function in FormResultCard

--- react-components/src/components/Form/FormResultCard.tsx
+++ react-components/src/components/Form/FormResultCard.tsx
@@ -1,41 +1,30 @@
 import React from 'react';
 import { FormCard } from '../../types';
 
-type Props = {
-  card: FormCard;
-};
-
-export class FormResultCard extends React.Component<Props> {
-  card: FormCard;
-  constructor(props: Props) {
-    super(props);
-    this.card = this.props.card;
-  }
-
-  render() {
-    return (
-      <div className="form-card">
-        <div className="form-card__image">
-          <img src={this.card.image} alt={`photo by ${this.card.name}`} />
+export const FormResultCard = ({ card }: { card: FormCard }) => {
+  const { country, date, gender, image, name } = card;
+  return (
+    <div className="form-card">
+      <div className="form-card__image">
+        <img src={image} alt={`photo by ${name}`} />
+      </div>
+      <div className="form-card__content">
+        <div className="form-card__row">
+          <div className="form-card__name">{name}</div>
         </div>
-        <div className="form-card__content">
-          <div className="form-card__row">
-            <div className="form-card__name">{this.card.name}</div>
-          </div>
-          <div className="form-card__row">
-            <div className="form-card__country">{this.card.country}</div>
-          </div>
-          <div className="form-card__row">
-            <div className="form-card__date">{this.card.date}</div>
-            <div className="form-card__gender">{this.card.gender}</div>
-          </div>
-          <div className="form-card__row">
-            <div className="form-card__agreement">
-              The user agrees to the processing of personal data
-            </div>
+        <div className="form-card__row">
+          <div className="form-card__country">{country}</div>
+        </div>
+        <div className="form-card__row">
+          <div className="form-card__date">{date}</div>
+          <div className="form-card__gender">{gender}</div>
+        </div>
+        <div className="form-card__row">
+          <div className="form-card__agreement">
+            The user agrees to the processing of personal data
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
