768516367422a72c11fd722f845545d019d10a7f
Alexandra Samoilova <avsamoilava@gmail.com>
2023-03-29 19:45:11 +0300

    feat: change class on function in Layout

--- react-components/src/components/layout/Layout.tsx
+++ react-components/src/components/layout/Layout.tsx
@@ -2,25 +2,12 @@ import React from 'react';
 import { Footer } from './Footer';
 import { Header } from './Header';
 
-type LayoutProps = {
-  children: React.ReactNode;
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
 };
-
-export class Layout extends React.Component {
-  children: React.ReactNode;
-
-  constructor(props: LayoutProps) {
-    super(props);
-    this.children = props.children;
-  }
-
-  render() {
-    return (
-      <>
-        <Header />
-        {this.children}
-        <Footer />
-      </>
-    );
-  }
-}
