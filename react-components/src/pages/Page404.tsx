f9e1af96f595c60570d15de93d882fa94497478b
Alexandra Samoilova <avsamoilava@gmail.com>
2023-03-29 19:41:31 +0300

    feat: change class on function in Page404

--- react-components/src/pages/Page404.tsx
+++ react-components/src/pages/Page404.tsx
@@ -1,27 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PageProps } from '../types';
 
-export class Page404 extends React.Component<PageProps> {
-  constructor(props: PageProps) {
-    super(props);
-  }
+export const Page404 = (props: PageProps) => {
+  useEffect(() => {
+    document.title = props.title;
+  }, [props.title]);
 
-  render() {
-    return (
-      <div className="page page-404">
-        <h1>404</h1>
-        <h2>nothig founds</h2>
-        <h3>all dust and ash</h3>
-        <h4>and hopelessness...</h4>
-      </div>
-    );
-  }
-
-  componentDidMount(): void {
-    document.title = this.props.title;
-  }
-
-  componentWillUnmount(): void {
-    document.title = '';
-  }
-}
+  return (
+    <div className="page page-404">
+      <h1>404</h1>
+      <h2>nothig founds</h2>
+      <h3>all dust and ash</h3>
+      <h4>and hopelessness...</h4>
+    </div>
+  );
+};
