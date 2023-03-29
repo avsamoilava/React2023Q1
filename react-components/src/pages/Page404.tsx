e27935484f9aad5713fc1533c13efc384163b0ab
Alexandra Samoilova <avsamoilava@gmail.com>
2023-03-29 20:22:05 +0300

    feat: add links, change class on function in footer,

--- react-components/src/pages/Page404.tsx
+++ react-components/src/pages/Page404.tsx
@@ -9,7 +9,7 @@ export const Page404 = (props: PageProps) => {
   return (
     <div className="page page-404">
       <h1>404</h1>
-      <h2>nothig founds</h2>
+      <h2>nothing founds</h2>
       <h3>all dust and ash</h3>
       <h4>and hopelessness...</h4>
     </div>
