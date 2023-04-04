abce1851999cbc472f8adac93557de62b07b4869
Alexandra Samoilova <avsamoilava@gmail.com>
2023-04-04 14:58:44 +0300

    fix: correct card test

--- react-components/src/__tests__/Card.test.tsx
+++ react-components/src/__tests__/Card.test.tsx
@@ -1,39 +1,56 @@
 import { render, screen } from '@testing-library/react';
-import { Product } from 'data/data';
 import { Card } from '../components/Card/Card';
+import { Char } from '../types';
 
-const fakeProduct: Product = {
-  id: 1,
-  title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-  price: 109.95,
-  description:
-    'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-  category: "men's clothing",
-  image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-  rating: {
-    rate: 3.9,
-    count: 120,
+const fakeChar: Char = {
+  id: 361,
+  name: 'Toxic Rick',
+  status: 'Dead',
+  species: 'Humanoid',
+  type: "Rick's Toxic Side",
+  gender: 'Male',
+  origin: {
+    name: 'Alien Spa',
+    url: 'https://rickandmortyapi.com/api/location/64',
   },
+  location: {
+    name: 'Earth',
+    url: 'https://rickandmortyapi.com/api/location/20',
+  },
+  image: 'https://rickandmortyapi.com/api/character/avatar/361.jpeg',
+  episode: ['https://rickandmortyapi.com/api/episode/27'],
+  url: 'https://rickandmortyapi.com/api/character/361',
+  created: '2018-01-10T18:20:41.703Z',
 };
 
 describe('Card component', () => {
-  it('should have a title', () => {
-    render(<Card product={fakeProduct} />);
-    expect(screen.getByText(fakeProduct.title)).toBeInTheDocument();
+  it('should have a name', () => {
+    render(<Card char={fakeChar} />);
+    expect(screen.getByText(fakeChar.name)).toBeInTheDocument();
   });
 
-  it('should have a category', () => {
-    render(<Card product={fakeProduct} />);
-    expect(screen.getByText(fakeProduct.category)).toBeInTheDocument();
+  it('should have a status', () => {
+    render(<Card char={fakeChar} />);
+    expect(screen.getByText(fakeChar.status)).toBeInTheDocument();
   });
 
-  it('should have a price', () => {
-    render(<Card product={fakeProduct} />);
-    expect(screen.getByText(`${fakeProduct.price}€`)).toBeInTheDocument();
+  it('should have a species', () => {
+    render(<Card char={fakeChar} />);
+    expect(screen.getByText(fakeChar.species)).toBeInTheDocument();
+  });
+
+  it('should have a gender', () => {
+    render(<Card char={fakeChar} />);
+    expect(screen.getByText(fakeChar.gender)).toBeInTheDocument();
+  });
+
+  it('should have a type', () => {
+    render(<Card char={fakeChar} />);
+    expect(screen.getByText(fakeChar.type)).toBeInTheDocument();
   });
 
   it('should have a picture', () => {
-    render(<Card product={fakeProduct} />);
-    expect(screen.getByRole('img')).toHaveAttribute('src', fakeProduct.image);
+    render(<Card char={fakeChar} />);
+    expect(screen.getByRole('img')).toHaveAttribute('src', fakeChar.image);
   });
 });
