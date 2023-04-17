263b599630e950bd3219cec344269a954dc1b057
Alexandra Samoilova <avsamoilava@gmail.com>
2023-04-17 20:59:39 +0300

    feat: add cross-fetch, correct setup tests

--- react-components/setupTests.js
+++ react-components/setupTests.js
@@ -4,7 +4,12 @@ import matchers from '@testing-library/jest-dom/matchers';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { mock } from './src/__tests__/mockData';
+import { fetch, Headers, Request, Response } from 'cross-fetch';
 
+global.fetch = fetch;
+global.Headers = Headers;
+global.Request = Request;
+global.Response = Response;
 expect.extend(matchers);
 
 const server = setupServer(
